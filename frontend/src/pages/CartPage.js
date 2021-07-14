d6ef7a6eac72f16022354b4cdf0b773caa6d8792
sb <stephane.baudry.vanaa@gmail.com>
2021-07-14 16:25:41 +0200

    added cartitem and cartpage

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,11 +1,27 @@
 import './CartPage.css'
 
-function CartPage() {
-  return (
-    <div className="cartpage">
-      cartpage
-    </div>
-  )
-}
+//components
+import CartItem from '../components/CartItem';
 
-export default CartPage
+const CartPage = () => {
+  return (
+  <div className="cartpage">
+    <div className="cartpage__left">
+      <h2>Shopping Cart</h2>
+
+      <CartItem />
+    </div>
+    <div className="cartpage__right">
+      <div className="cartscreen_info">
+        <p>Subtotal (0) items</p>
+        <p>$99</p>
+      </div>
+      <div>
+        <button>Proceed To Checkout</button>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default CartPage;
